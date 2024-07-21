8cc6e60b8fa20b7fc700b56841b462e223290dd3
Diana de Oliveira Martins <99148060+ddianaom@users.noreply.github.com>
2024-07-21 17:58:00 -0400

    Share Exams

---
+++ contracts/BloodExams.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract BloodExams {
+    struct BloodTest {
+        uint id;
+        address payable owner;
+        string ipfsHash; // hash do IPFS do exame de sangue
+        address[] sharedWith; // Lista de endereços com quem o exame é compartilhado
+    }
+
+    mapping(uint => BloodTest) public bloodTests;
+    uint public bloodTestCount = 0;
+
+    event BloodTestCreated(
+        uint id,
+        address payable owner,
+        string ipfsHash
+    );
+
+    event AccessGranted(
+        uint id,
+        address grantedTo
+    );
+
+    function createBloodTest(string memory _ipfsHash) public {
+        bloodTestCount ++;
+        bloodTests[bloodTestCount] = BloodTest(bloodTestCount, payable(msg.sender), _ipfsHash, new address[](0));
+        emit BloodTestCreated(bloodTestCount, payable(msg.sender), _ipfsHash);
+    }
+
+    function grantAccess(uint _id, address _grantedTo) public {
+        BloodTest storage _bloodTest = bloodTests[_id];
+        require(_bloodTest.id > 0 && _bloodTest.id <= bloodTestCount, "Blood test does not exist");
+        require(_bloodTest.owner == msg.sender, "Only the owner can grant access");
+        require(_grantedTo != address(0), "Invalid address");
+
+        _bloodTest.sharedWith.push(_grantedTo);
+        emit AccessGranted(_id, _grantedTo);
+    }
+
+    function getSharedWith(uint _id) public view returns (address[] memory) {
+        BloodTest storage _bloodTest = bloodTests[_id];
+        require(_bloodTest.id > 0 && _bloodTest.id <= bloodTestCount, "Blood test does not exist");
+        return _bloodTest.sharedWith;
+    }
+}
